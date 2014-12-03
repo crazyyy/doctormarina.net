e78908b113330d13a311a413167fdee1cfd2b6ce
Vitaly A <crazyyy@gmail.com>
2014-12-03 15:28:11 +0200

    go to js

--- www/wp-content/themes/adaption/header.php
+++ www/wp-content/themes/adaption/header.php
@@ -23,14 +23,16 @@
 	<?php do_action( 'before' ); ?>
 
 	<div id="mobile-panel">
-		<?php if ( ( has_nav_menu( 'social' ) ) || ( has_nav_Menu( 'primary') ) ) : ?>
-			<div id="mobile-link"></div><!-- #mobile-link -->
-		<?php endif; ?>
-		<?php if ( is_active_sidebar( 'sidebar-1' ) || is_active_sidebar( 'sidebar-2' ) ) : ?>
-			<div id="widget-link"></div><!-- #widget-link -->
-		<?php endif; ?>
+		<?php
+		if ( has_nav_menu( 'headerok' ) ) : ?>
+			<div id="headerok-nav" class="headerok-small">
+				<?php wp_nav_menu( array(
 
-		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				) ); ?>
+			</div><!-- headerok-nav -->
+		<?php endif; ?>
+		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Доктор Марина Невдовец</a></h1>
+		<h2 class="site-description">профессиональный косметолог</h2>
 	</div>
 
 	<div id="panel-block">
--- www/wp-content/themes/adaption/index.php
+++ www/wp-content/themes/adaption/index.php
@@ -14,21 +14,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<aside class="headerok entry-meta">
-			<!-- <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1> -->
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Доктор Марина Невдовец</a></h1>
-			<!-- <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2> -->
-			<h2 class="site-description">профессиональный косметолог</h2>
-			<?php
-			if ( has_nav_menu( 'headerok' ) ) : ?>
-				<div id="headerok-nav" class="headerok-nav">
-					<?php wp_nav_menu( array(
-
-					) ); ?>
-				</div><!-- headerok-nav -->
-			<?php endif; ?>
-			<a href="tel:+380687438946" class="head-phone">(068) 743 89 46</a>
-		</aside><!-- headerok entry-meta -->
+		<?php get_template_part('headerok'); ?>
 		<main id="main" class="site-main" role="main">
 
 			<?php if ( have_posts() ) : ?>
